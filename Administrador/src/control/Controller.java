2fcd232a78dc8ade23856e9cda81a22cf4518c91
FERNANDO BORDIN LOPES <unifflopes@feinet.fei.edu.br>
2025-04-24 15:04:08 -0300

    Arquivos alterados

--- Administrador/src/control/Controller.java
+++ Administrador/src/control/Controller.java
@@ -1,19 +1,13 @@
-package gui;
-
-import view.Janela;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package control;
 
 /**
  *
  * @author unifflopes
  */
-public class Principal {
-    public static void main (String[] agrs){
-        Janela janela = new Janela();
-        janela.setVisible(true);
-    }
+public class Controller {
+    
 }
